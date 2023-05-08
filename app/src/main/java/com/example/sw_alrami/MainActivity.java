35795908cc4e4db060087d759f622bcb95a3ebc0
beomjunpark <park97111997@gmail.com>
2023-05-09 02:17:23 +0900

    내비게이션바 구현 수정

--- app/src/main/java/com/example/sw_alrami/MainActivity.java
+++ app/src/main/java/com/example/sw_alrami/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.sw_alrami;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mBottomNV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNV = findViewById(R.id.nav_view);
+        mBottomNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() { //NavigationItemSelecte
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                BottomNavigate(menuItem.getItemId());
+
+
+                return true;
+            }
+        });
+        mBottomNV.setSelectedItemId(R.id.tab_notice);
     }
+    private void BottomNavigate(int id) {  //BottomNavigation 페이지 변경
+        String tag = String.valueOf(id);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+        if (currentFragment != null) {
+            fragmentTransaction.hide(currentFragment);
+        }
+
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment == null) {
+            if (id == R.id.tab_notice) {
+                fragment = new FragmentPage1();
+
+            } else if (id == R.id.tab_job){
+
+                fragment = new FragmentPage2();
+            } else if (id == R.id.tab_news){
+
+                fragment = new FragmentPage3();
+            } else if (id == R.id.tab_bookmark){
+
+                fragment = new FragmentPage4();
+            }else {
+                fragment = new FragmentPage5();
+            }
+
+            fragmentTransaction.add(R.id.content_layout, fragment, tag);
+        } else {
+            fragmentTransaction.show(fragment);
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
+
+
+    }
+
 }
